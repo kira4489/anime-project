bd01c3dd6ebf69fa2c4d53f4237a698a113a55cf
jhonatanquirama <quiramapino47@gmail.com>
2021-01-21 13:15:30 -0500

    adding js api and params

---
+++ public/js/details.js
@@ -0,0 +1,43 @@
+let details = document.getElementById("details");
+
+// console.log(id);
+// recuperamos el querystring
+const id = window.location.search.substr(1).replace("id=", "");
+console.log(id); // '?q=pisos+en+barcelona&ciudad=Barcelona'
+
+// usando el querystring, creamos un objeto del tipo URLSearchParams
+const params = new URLSearchParams(window.location.search);
+console.log(params);
+
+function text() {
+  return fetch(`https://api.jikan.moe/v3/anime/${id}`)
+    .then((response) => response.json())
+    .catch((error) => console.log(error.message));
+}
+
+async function lorem() {
+  let response = await text();
+  console.log(response);
+
+  details.innerHTML = `
+  <h1 class="title">${response.title}</h1>
+  <div class="anime">
+        <div class="anime__illustration">
+          <figure class="anime__figure">
+            <img class="anime__img" src=${response.image_url} alt="anime image">
+          </figure>
+        </div>
+        <div class="anime__info">
+          <div class="anime-pill">
+              <p class="anime-pill__pill anime-pill__pill--top">top</p>
+              <p class="anime-pill__pill anime-pill__pill--gender">gender</p>
+              <p class="anime-pill__pill anime-pill__pill--season">season</p>
+
+          </div>
+          <p class="anime__details">${response.synopsis}</p>
+        </div>
+      </div>
+  `;
+}
+
+lorem();
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,32 @@
+let contenido = document.getElementById("contenido");
 
+function traer() {
+  fetch("https://api.jikan.moe/v3/genre/anime/2/1", {
+    method: "get",
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.anime.map((item, index) => {
+        //  console.log(item)
+        if (index > 8) {
+          return;
+        }
+        contenido.innerHTML += `
+     <div class="card">
+     <img class="card__img" src=${item.image_url}>
+     <div class="card-info">
+         <div class="card-header">
+             <h2 class="card-header__title">${item.title}</h2>
+             <p class="card-header__pill">${item.type}</p>
+         </div>
+         <div class="card-footer">
+             <button class="card-footer__btn"><a class= "card-footer__link" href="/public/views/details.html?id=${item.mal_id}">Read more >></a></button>
+         </div>
+         </div>
+         </div>
+     `;
+      });
+    });
+}
+
+traer();
