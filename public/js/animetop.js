b3c4baab93af07a0a2ec1253512a85d330d50494
Aman=sgz <67084595+Aman-sgz@users.noreply.github.com>
2021-01-23 11:27:02 -0300

    Add responsive to anime top

--- public/js/animetop.js
+++ public/js/animetop.js
@@ -13,24 +13,27 @@ function Numberten() {
     if (index > 20 && index < 30) {
       console.log(index)
       return (ran.innerHTML += `
-    <section class="ranking">
-      <div class="ranking__left">
+        <section class="ranking">
           <div class="ranking__header">
-              <p class="ranking__number">1</p>
-              <h2 class="ranking__title">${item.title}</h2>
+              <p class="ranking__number">#1</p>
+              <h2 class="ranking__title">${item.title}</h2>    
           </div>
-          <figure class="ranking__figure">
-              <img class="ranking__img" src=${item.image_url} alt="anime image">
-          </figure>
-      </div>
-      <div class="ranking__right">
-          <div class="anime-pill">
-              <p class="anime-pill__pill anime-pill__pill--genre">${item.genres[2].name}</p>
-              <p class="anime-pill__pill anime-pill__pill--type">${item.type}</p>
+
+          <div class= "ranking__container">
+            <div class="ranking__left">
+              <figure class="ranking__figure">
+                  <img class="ranking__img" src=${item.image_url} alt="anime image">
+              </figure>
+            </div>
+            <div class="ranking__right">
+              <div class="anime-pill">
+                <p class="anime-pill__pill anime-pill__pill--genre">${item.genres[2].name}</p>
+                <p class="anime-pill__pill anime-pill__pill--type">${item.type}</p>
+              </div>
+                <p class="ranking__details">${item.synopsis}</p>
+            </div>
           </div>
-          <p class="ranking__details">${item.synopsis}</p>
-      </div>           
-</section>
+        </section>
         `);
     }
   });
