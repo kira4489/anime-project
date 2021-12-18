e3807c8945e9d5818a8d04c922e0bf9c7987a9ed
jhonatanquirama <quiramapino47@gmail.com>
2021-12-18 15:14:37 -0500

    fix slider

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,10 +1,6 @@
-var swiper = new Swiper(".swiper-container", {
-    pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-    },
+var swiper = new Swiper(".mySwiper", {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-});
+  });
